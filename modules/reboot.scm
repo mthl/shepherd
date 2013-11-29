3bef41308a8febbc5396a6a29a41cdf5b9407961
Ludovic Courtès <ludo@gnu.org>
2013-11-29 22:16:14 +0100

    Add 'halt' and 'reboot' commands.
    * halt.in, reboot.in, modules/halt.scm, modules/reboot.scm: New files.
    * Makefile.am (sbin_SCRIPTS): New variable.
      (templates): Add halt.in and reboot.in.
      (dist_dmd_DATA): Add halt.scm and reboot.scm.
      (maybe_executable): New variable.
      (%:%.in): Use it.
      (deco, dmd): Remove targets.

---
+++ modules/reboot.scm
@@ -0,0 +1,60 @@
+;; reboot.scm -- Reboot the system.
+;; Copyright (C) 2013 Ludovic Courtès <ludo@gnu.org>
+;;
+;; This file is part of GNU dmd.
+;;
+;; GNU dmd is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; GNU dmd is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU dmd.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (reboot)
+  #:use-module (dmd support)
+  #:use-module (dmd args)
+  #:use-module (dmd comm)
+  #:use-module (oop goops)
+  #:use-module (ice-9 rdelim)
+  #:export (program-name
+            main))
+
+(define program-name "reboot")
+
+
+
+;; Main program.
+(define (main . args)
+  (false-if-exception (setlocale LC_ALL ""))
+
+  (let ((socket-file default-socket-file)
+	(command-args '()))
+    (process-args program-name args
+		  "ARG..."
+		  "Reboot the system."
+		  not ;; Fail on unknown args.
+		  (make <option>
+		    #:long "socket" #:short #\s
+		    #:takes-arg? #t #:optional-arg? #f #:arg-name "FILE"
+		    #:description "send commands to FILE"
+		    #:action (lambda (file)
+			       (set! socket-file file))))
+
+    (set! command-args (reverse command-args))
+    (let ((sock (open-connection socket-file)))
+      ;; Send the command without further ado.
+      (write-command (dmd-command 'stop 'dmd) sock)
+
+      ;; Receive output.
+      (setvbuf sock _IOLBF)
+      (let loop ((line (read-line sock)))
+        (unless (eof-object? line)
+          (display line)
+          (newline)
+          (loop (read-line sock)))))))
