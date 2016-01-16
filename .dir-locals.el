e3d2f9a63752322cf2c64fee46513ca5852b4ffe
Mathieu Lirzin <mthl@gnu.org>
2016-01-16 23:17:39 +0100

    maint: Add .dir-locals.el.
    * .dir-locals.el: New file.
    
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

---
+++ .dir-locals.el
@@ -0,0 +1,13 @@
+;; Per-directory local variables for GNU Emacs 23 and later.
+
+((nil
+  . ((fill-column . 78)
+     (tab-width   .  8)
+
+     ;; For use with 'bug-reference-prog-mode'.
+     (bug-reference-url-format . "http://bugs.gnu.org/%s")
+     (bug-reference-bug-regexp
+      . "<https?://\\(debbugs\\|bugs\\)\\.gnu\\.org/\\([0-9]+\\)>")))
+ (scheme-mode     . ((indent-tabs-mode . nil)))
+ (texinfo-mode    . ((indent-tabs-mode . nil)
+                     (fill-column . 72))))
