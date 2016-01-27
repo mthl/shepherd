mthl/shepherd

commit cb168150d4581ffbc357ab6cdcd45d59051ee821

service: Rewrite 'conflicts-with' in functional style.
* modules/shepherd/service.scm (conflicts-with): Rewrite using
'append-map' and 'filter-map'.
(conflicts-with-running): Rewrite in terms of 'conflicts-with'.

modules/shepherd/service.scm
@@ -380,33 +380,20 @@ respawned, shows that it has been respawned more than TIMES in SECONDS."
 ;; Return a list of canonical names of the services that conflict with
 ;; OBJ.
 (define-method (conflicts-with (obj <service>))
-  (let ((conflicts '()))
-    (for-each (lambda (sym)
-		(for-each (lambda (s)
-			    (set! conflicts (cons (canonical-name s)
-						  conflicts)))
-			  (lookup-services sym)))
-	      (provided-by obj))
-    ;; Clean up the result.
-    (delete! (canonical-name obj)
-	     (delete-duplicates! conflicts eq?)
-	     eq?)))
+  (delete-duplicates
+   (append-map (lambda (sym)
+                 (filter-map (lambda (service)
+                               (and (not (eq? service obj))
+                                    (canonical-name service)))
+                             (lookup-services sym)))
+               (provided-by obj))
+   eq?))
 
 ;; Check if this service provides a symbol that is already provided
 ;; by any other running services.  If so, return the canonical names
 ;; of the other services.  Otherwise, return the empty list.
 (define-method (conflicts-with-running (obj <service>))
-  (let ((conflicts '()))
-    (for-each-service
-     (lambda (serv)
-       (and (running? serv)
-	    (for-each (lambda (sym)
-			(and (memq sym (provided-by obj))
-			     (set! conflicts
-				   (cons (canonical-name serv)
-					 conflicts))))
-		      (provided-by serv)))))
-    conflicts))
+  (filter running? (conflicts-with obj)))
 
 ;; Start OBJ, but first kill all services which conflict with it.
 ;; FIXME-CRITICAL: Conflicts of indirect dependencies.  For this, we

commit 88c6a20b895c41b8998ee298165e82dcba2d16e8

service: 'conflicts-with' returns a list of services.
This fixes a regression introduced in commit
cb168150d4581ffbc357ab6cdcd45d59051ee821.

If 'conflicts-with' returns a list of names, 'conflicts-with-running'
fails because 'running?' cannot be applied to a symbol.

* modules/shepherd/service.scm (conflicts-with): Return a list of
  services instead of names.
  (start): Adjust accordingly.

modules/shepherd/service.scm
@@ -287,7 +287,8 @@ wire."
 	((let ((conflicts (conflicts-with-running obj)))
 	   (or (null? conflicts)
 	       (local-output "Service ~a conflicts with running services ~a."
-			     (canonical-name obj) conflicts))
+			     (canonical-name obj)
+			     (map canonical-name conflicts)))
 	   (not (null? conflicts)))
 	 #f) ;; Dummy.
 	(else
@@ -451,21 +452,20 @@ wire."
        ;; FIXME: Implement doc-help.
        (local-output "Unknown keyword.  Try 'doc root help'.")))))
 
-;; Return a list of canonical names of the services that conflict with
-;; OBJ.
+;; Return a list of services that conflict with OBJ.
 (define-method (conflicts-with (obj <service>))
   (delete-duplicates
    (append-map (lambda (sym)
                  (filter-map (lambda (service)
                                (and (not (eq? service obj))
-                                    (canonical-name service)))
+                                    service))
                              (lookup-services sym)))
                (provided-by obj))
    eq?))
 
 ;; Check if this service provides a symbol that is already provided
-;; by any other running services.  If so, return the canonical names
-;; of the other services.  Otherwise, return the empty list.
+;; by any other running services.  If so, return these services.
+;; Otherwise, return the empty list.
 (define-method (conflicts-with-running (obj <service>))
   (filter running? (conflicts-with obj)))
 