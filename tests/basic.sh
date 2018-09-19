mthl/shepherd

commit 329cec8b6fcc57f945e748793470f573b2b449a3

shepherd: Preserve empty lines in action messages.
Previously, "herd hi test-2" in the example added by this commit would
lack the empty line between "start" and "end".

* modules/shepherd.scm (%not-newline): Remove.
(process-command)[get-messages]: Use 'string-split' plus some
post-processing instead of 'string-tokenize'.
* modules/shepherd/comm.scm (%not-newline): Remove.
(make-shepherd-output-port): Use 'string-split' instead of
'string-tokenize'.
* tests/basic.sh: Add #:actions in 'test-2' and test the new action.

modules/shepherd/comm.scm
@@ -282,9 +282,6 @@ available."
       (call-with-syslog-port close-port)))
    "w"))                                          ;output port
 
-(define %not-newline
-  (char-set-complement (char-set #\newline)))
-
 ;; We provide our own output mechanism, because we have certain
 ;; special needs; most importantly, we want to send output to herd
 ;; sometimes.
@@ -312,7 +309,7 @@ available."
         (if (not (string-index str #\newline))
             (set! buffer (cons str buffer))
             (let* ((str   (string-concatenate-reverse (cons str buffer)))
-                   (lines (string-tokenize str %not-newline)))
+                   (lines (string-split str #\newline)))
               (define prefix
                 (strftime (%current-logfile-date-format)
                           (localtime (current-time))))

tests/basic.sh
@@ -1,5 +1,5 @@
 # GNU Shepherd --- Test basic communication capabilities.
-# Copyright © 2013, 2014, 2016, 2017 Ludovic Courtès <ludo@gnu.org>
+# Copyright © 2013, 2014, 2016, 2017, 2018 Ludovic Courtès <ludo@gnu.org>
 # Copyright © 2016 Mathieu Lirzin <mthl@gnu.org>
 # Copyright © 2014 Alex Sassmannshausen <alex.sassmannshausen@gmail.com>
 #
@@ -54,6 +54,10 @@ cat > "$conf"<<EOF
              #t)
    #:stop  (lambda _
              (delete-file "$stamp-2"))
+   #:actions (make-actions (hi "Say hi."
+                               (lambda _
+                                 (display "start\n\nend\n")
+                                 #t)))
    #:respawn? #f)
  (make <service>
    #:provides '(broken)
@@ -93,6 +97,14 @@ then false; else true; fi
 
 $herd enable test-2
 $herd start test-2
+
+# Try a custom action; make sure we get all the lines, including the empty
+# lines (this was not the case in 0.4.0.)
+$herd doc test-2 action hi | grep "Say hi\."
+$herd hi test-2
+$herd hi test-2 | grep '^start$'
+$herd hi test-2 | grep '^end$'
+test `$herd hi test-2 | wc -l` -eq 3
 
 # This used to crash shepherd: <http://bugs.gnu.org/24684>.
 if $herd enable test-2 with extra arguments

modules/shepherd.scm
@@ -284,9 +284,6 @@
       ;; Maybe we got EPIPE while writing to SOCK, or something like that.
       (false-if-exception (close sock)))))
 
-(define %not-newline
-  (char-set-complement (char-set #\newline)))
-
 (define (process-command command port)
   "Interpret COMMAND, a command sent by the user, represented as a
 <shepherd-command> object.  Send the reply to PORT."
@@ -302,8 +299,19 @@
              (open-output-string)))
 
          (define (get-messages)
-           (string-tokenize (get-output-string message-port)
-                            %not-newline))
+           (let* ((str (get-output-string message-port))
+                  (lst (string-split str #\newline)))
+             ;; 'string-tokenize' swallows empty lines, which is not great,
+             ;; and 'string-split' doesn't distinguish between an empty line
+             ;; and this empty string, which is not great either.  So we hack
+             ;; our way the best we can.
+             (cond ((string-null? str)
+                    '())
+                   ;; If STR ends in \n, drop the trailing empty string since
+                   ;; that would lead the client to print an extra newline.
+                   ((string-suffix? "\n" str)
+                    (drop-right lst 1))
+                   (else lst))))
 
          (parameterize ((%current-client-socket message-port))
            (guard (c ((service-error? c)

commit e45820839e23a19a00347d3b8f88206243fb3eb0

comm: Do not introduce empty lines in the log.
Fixes a regression introduced in
329cec8b6fcc57f945e748793470f573b2b449a3 whereby regular "has been
started" etc. lines in the log would be followed by an empty line.

* modules/shepherd/comm.scm (make-shepherd-output-port): Use
'string-index' in a loop instead of 'string-split' to detect newlines in
STR.
* tests/basic.sh: Check for zero empty lines in $log.

modules/shepherd/comm.scm
@@ -308,18 +308,23 @@ available."
         ;; completed line.
         (if (not (string-index str #\newline))
             (set! buffer (cons str buffer))
-            (let* ((str   (string-concatenate-reverse (cons str buffer)))
-                   (lines (string-split str #\newline)))
+            (let ((str (string-concatenate-reverse (cons str buffer))))
               (define prefix
                 (strftime (%current-logfile-date-format)
                           (localtime (current-time))))
 
-              ;; Make exactly one 'display' call per line to make sure we
-              ;; don't create several entries for each line.
-              (for-each (lambda (line)
-                          (display (string-append prefix line "\n")
-                                   (log-output-port)))
-                        lines)
+              ;; Note: We want to render as many newlinew as present in STR,
+              ;; so neither 'string-split' nor 'string-tokenize' helps.
+              (let loop ((str str))
+                (let* ((index (string-index str #\newline))
+                       (line  (if index (string-take str (+ 1 index)) str)))
+                  (unless (string-null? str)
+                    ;; Make exactly one 'display' call per line to make sure we
+                    ;; don't create several entries for each line.
+                    (display (string-append prefix line) (log-output-port))
+                    (when index
+                      (loop (string-drop str (+ index 1)))))))
+
               (set! buffer '())))))
 
     ;; Flush output.

tests/basic.sh
@@ -98,6 +98,11 @@ then false; else true; fi
 $herd enable test-2
 $herd start test-2
 
+# Make sure we didn't emit empty lines in the log (strip the timestamp that
+# prefixes each line.)
+test `wc -l < "$log"` -gt 0
+test `cut -c 21- < "$log" | grep "^$" | wc -l` -eq 0
+
 # Try a custom action; make sure we get all the lines, including the empty
 # lines (this was not the case in 0.4.0.)
 $herd doc test-2 action hi | grep "Say hi\."